4cd92d678eeef083fdf0292c30339540ba11f346
RoguePY <40289304+RoguePY@users.noreply.github.com>
2019-04-18 02:51:19 -0500

    Update client.py

--- client.py
+++ client.py
@@ -44,7 +44,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	word = message.content.lower()
-	for word in automod:
+	if word in automod:
 		channel = message.channel
 		await message.delete()
 		await channel.send(message.author.mention + "*, no need to say that!*")
